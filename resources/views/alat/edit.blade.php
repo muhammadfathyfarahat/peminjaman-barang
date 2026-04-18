f2b31628a765966e5741f6ce41f810429217e132
muhammadfathyfarahat <muhammadfathy625@gmail.com>
2026-04-19 01:12:41 +0700

    frontend: integrate adminlte template

--- resources/views/alat/edit.blade.php
+++ resources/views/alat/edit.blade.php
@@ -4,31 +4,35 @@
 
 <h2>Data Alat</h2>
 
-<table class="table">
-...
+<a href="/alat/create" class="btn btn-primary mb-2">Tambah</a>
+
+<table class="table table-bordered">
+    <tr>
+        <th>Nama</th>
+        <th>Kategori</th>
+        <th>Stok</th>
+        <th>Kondisi</th>
+        <th>Aksi</th>
+    </tr>
+
+    @foreach($alat as $a)
+    <tr>
+        <td>{{ $a->nama }}</td>
+        <td>{{ $a->kategori->nama }}</td>
+        <td>{{ $a->stok }}</td>
+        <td>{{ $a->kondisi }}</td>
+        <td>
+            <a href="/alat/{{ $a->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+
+            <form action="/alat/{{ $a->id }}" method="POST" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-sm">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+
 </table>
 
 @endsection
-<h2>Edit Alat</h2>
-
-<form method="POST" action="/alat/{{ $alat->id }}">
-    @csrf
-    @method('PUT')
-
-    <input name="nama" value="{{ $alat->nama }}"><br>
-
-    <select name="kategori_id">
-        @foreach($kategori as $k)
-        <option value="{{ $k->id }}" 
-            {{ $alat->kategori_id == $k->id ? 'selected' : '' }}>
-            {{ $k->nama }}
-        </option>
-        @endforeach
-    </select><br>
-
-    <input name="stok" value="{{ $alat->stok }}"><br>
-
-    <input name="kondisi" value="{{ $alat->kondisi }}"><br>
-
-    <button>Update</button>
-</form>
--- resources/views/kategori/edit.blade.php
+++ resources/views/kategori/edit.blade.php
@@ -4,18 +4,35 @@
 
 <h2>Data Alat</h2>
 
-<table class="table">
-...
+<a href="/alat/create" class="btn btn-primary mb-2">Tambah</a>
+
+<table class="table table-bordered">
+    <tr>
+        <th>Nama</th>
+        <th>Kategori</th>
+        <th>Stok</th>
+        <th>Kondisi</th>
+        <th>Aksi</th>
+    </tr>
+
+    @foreach($alat as $a)
+    <tr>
+        <td>{{ $a->nama }}</td>
+        <td>{{ $a->kategori->nama }}</td>
+        <td>{{ $a->stok }}</td>
+        <td>{{ $a->kondisi }}</td>
+        <td>
+            <a href="/alat/{{ $a->id }}/edit" class="btn btn-warning btn-sm">Edit</a>
+
+            <form action="/alat/{{ $a->id }}" method="POST" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger btn-sm">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+
 </table>
 
 @endsection
-<h2>Edit Kategori</h2>
-
-<form method="POST" action="/kategori/{{ $kategori->id }}">
-    @csrf
-    @method('PUT')
-
-    <input name="nama" value="{{ $kategori->nama }}"><br>
-
-    <button>Update</button>
-</form>
